3edee969a7980d9162f61ad9e2e5778140b2d874
Prerna Gupta <56025548+prerna2712@users.noreply.github.com>
2021-05-14 16:48:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-import datetime
+from datetime import datetime
 import pyttsx3
+import pytz
 
-def speak(text):
+def speak(s):
+    text = str(s)
     engine = pyttsx3.init()
     engine.setProperty("rate",150)
     engine.say(text)
     engine.runAndWait()
 
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+def get_date():
+    now = datetime.now()
+    today = now.strftime("%d/%m/%Y")
+    print(today)
+
+get_date()
+get_time()
 speak("hello")
+speak("take paracetamol")
