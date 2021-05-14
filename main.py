1720345d9c1bff38665fbcadbcdc41c38cec685d
Prerna Gupta <56025548+prerna2712@users.noreply.github.com>
2021-05-14 16:16:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import datetime
+import pyttsx3
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty("rate",150)
+    engine.say(text)
+    engine.runAndWait()
+
+speak("hello")
